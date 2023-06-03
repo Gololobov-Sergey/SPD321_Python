2b57f49731f5add59516abac5f4f4435c12a5899
Сергій Гололобов <gololobov.serhiy@gmail.com>
2023-06-03 16:41:52 +0300

    Добавьте файлы проекта.

---
+++ SPD321_Python/Lesson1.py
@@ -0,0 +1,19 @@
+﻿print("Hello", "C++", 4545, end=" *** ", sep=', ')
+print('Python')
+print(65656)
+print(2+3)
+print("Hello 'Python'")
+print('Hello "Python"')
+print("Hello \"Python\"")
+print("Hello \\Python\\")
+print("\\/\\/\\/")
+print("Menu:")
+print("1. Save..")
+print("1. Save..")
+print("1. Save..")
+
+print("Menu:\n1.Save..\n2.Exit\n3....")
+
+print("\t\t\tHello")
+
+print("Слава Україні!\a\a\a\a")
