c687ebced6f8ef26608f4ff74cc015f294d1f5c7
Сергій Гололобов <gololobov.serhiy@gmail.com>
2023-06-10 16:24:29 +0300

    10.06.2023

---
+++ SPD321_Python/Lesson2.py
@@ -0,0 +1,120 @@
+﻿
+
+'''#str
+print('Hello')
+
+#int
+print(123)
+print(11_111_111_111)
+print(0b101101)
+print(0o55)
+print(0x2d)
+
+#float
+print(12.32)
+print(0.0000000000000000000000000000001)
+print(0.0000000000000000000000000000671)
+print(1.23e30)
+
+#bool
+print(True)
+print(False)
+
+
+
+# a-z, A-Z, 0-9, _
+
+# sumGrn
+
+
+a = 1
+s = "mama"'''
+
+
+'''print("Програма для розрахунку площі прямокутника\nна підставі довжин двох сторін\n==========================")
+a = float(input("Введіть значення сторони а: "))
+b = float(input("Введіть значення сторони b: "))
+s = a * b
+print("Площа прямокутника зі сторонами а =", a, ", b =", b, "дорівнює", s)'''
+
+'''a = 2
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+print(2 * 3.)
+
+
+print(2 ** 3)
+
+
+print(2 / 3)
+print(2. / 3)
+print(2 / 3.)
+print(2. / 3.)
+
+
+print(2 // 3)
+print(2. // 3)
+print(2 // 3.)
+print(2. // 3.)
+
+
+
+print(-6 // 4)
+
+
+
+print(7.5 % 3.3)
+print(8 % 3)
+print(9 % 3)
+print(10 % 3)
+print(11 % 3)
+
+print(4 % 2)
+
+print(-a)
+print(+a)
+
+print((2+2)*2)
+
+print(5 + 3 % 2)'''
+
+
+# -
+# **
+# *,/,%,//
+# +, -
+
+
+'''a = float(input("A: "))
+b = float(input("B: "))
+c = float(input("C: "))
+ac = abs(c - a)
+bc = abs(b - c)
+r = ac * bc
+print(r)'''
+
+
+'''a = float(input("A: "))
+b = float(input("B: "))
+t = a
+a = b
+b = t
+print(a)
+print(b)'''
+
+
+'''a = int(input("A: "))
+d = a // 10
+o = a - d * 10 # o = a % 10
+print(d)
+print(o)'''
+
+# 123  312
+a = int(input("A: "))
+o = a % 10
+a = o * 100 + a // 10
+print(a)
